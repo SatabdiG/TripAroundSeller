a49b8531bdcf1407ba301b213e7c472863f74294
Shreya Soans <soans.shreya@gmail.com>
2017-01-16 16:38:47 +0100

    Pushing

--- public/FrontEnd/js/dynamic.js
+++ public/FrontEnd/js/dynamic.js
@@ -1,8 +1,23 @@
-function transla( id,  lang)
+function transla(id, lang)
 {
    var c;
+   var text_eng = [
+   "Description"," Select map"," Edit Map"," Delete Map"," Publish Map","Publish", "Select a map or create a new one."
+   ];
+   var text_ger = [
+   "Beschreibung"," Karte auswählen"," Karte bearbeiten"," Karte löschen"," Karte veröffentlichen"," Veröffentlichen", "Karte auswählen oder eine neue erstellen."
+   ];
+  /* if(lang=="German" || lang =="Deutsch"){
+        c = "Select mapss";
+
+   }*/
    if(lang=="German" || lang =="Deutsch"){
-        c = "Select map";
+        c = text_ger;
+        return c;
    }
-   return c;
+   else if(lang=="English" || lang =="Englisch"){
+        c = text_eng;
+        return c;
+   }
+
 }
